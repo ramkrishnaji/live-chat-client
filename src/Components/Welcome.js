08fbb16c9d417621f3a8ab404763a50e24cd2592
Rk <91599808+ramkrishnaji@users.noreply.github.com>
2023-11-20 00:25:14 +0530

    Add files via upload

---
+++ src/Components/Welcome.js
@@ -0,0 +1,32 @@
+import React from "react";
+import logo from "../Images/gamers_icon.png";
+  import { useSelector } from "react-redux";
+import { motion } from "framer-motion";
+import { useNavigate } from "react-router-dom";
+
+function Welcome() {
+  const lightTheme = useSelector((state) => state.themeKey);
+  const userData = JSON.parse(localStorage.getItem("userData"));
+  console.log(userData);
+  const nav = useNavigate();
+  if (!userData) {
+    console.log("User not Authenticated");
+    nav("/");
+  }
+
+  return (
+    <div className={"welcome-container" + (lightTheme ? "" : " dark")}>
+      <motion.img
+        drag
+        whileTap={{ scale: 1.05, rotate: 360 }}
+        src={logo}
+        alt="Logo"
+        className="welcome-logo"
+      />
+      <b>Welcome , {userData.data.name} 👋</b>
+      <p>Discuss and level up your gaming experience.</p>
+    </div>
+  );
+}
+
+export default Welcome;
